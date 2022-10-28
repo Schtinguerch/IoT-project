06f38b1ebd499ff8290f8bc6346ea50d44356829
Schtinguerch <schtinguerch@gmail.com>
2022-10-29 00:01:17 +0500

    Подключение шрифтов + класс для упрощённого доступа к иконкам

--- AndroidApp/FridgeSnitchClient/FridgeSnitchClient/AssemblyInfo.cs
+++ AndroidApp/FridgeSnitchClient/FridgeSnitchClient/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("fabr.otf", Alias = "fabr")]
+[assembly: ExportFont("far.otf", Alias = "far")]
+[assembly: ExportFont("fas.otf", Alias = "fas")]
