b5be36836ce443fd71c55634a327c5293596bbc2
Schtinguerch <schtinguerch@gmail.com>
2022-10-28 20:28:17 +0500

    Класс аутентификации пользователей

---
+++ AndroidApp/FridgeSnitchClient/FridgeSnitchClient/Authentification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FridgeSnitchClient
+{
+    public class Authentification
+    {
+        public bool IsAuthentificated { get; private set; } = false;
+        public readonly Validator Validator = new Validator();
+
+        public bool TryAuthentificate(string userName, string password)
+        {
+            var areDataValid = Validator.ValidateCredentials(ref userName, password);
+            if (!areDataValid)
+            {
+                IsAuthentificated = false;
+                return false;
+            }
+
+            //TODO: perform the request to the server
+            //Now returns true as a plug
+
+            var isAuthentificationSucceed = true;
+
+            IsAuthentificated = isAuthentificationSucceed;
+            return IsAuthentificated;
+        }
+    }
+}
