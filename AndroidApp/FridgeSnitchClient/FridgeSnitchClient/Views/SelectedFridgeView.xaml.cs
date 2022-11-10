7068b1cfeee31317ef7d38473657ced901edf41d
Schtinguerch <schtinguerch@gmail.com>
2022-11-10 20:25:50 +0500

    Представление выбранного холодильника для его настроек

---
+++ AndroidApp/FridgeSnitchClient/FridgeSnitchClient/Views/SelectedFridgeView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FridgeSnitchClient.Models;
+using FridgeSnitchClient.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FridgeSnitchClient.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectedFridgeView : ContentPage
+    {
+        public SelectedFridgeView(FridgeModel selectedFridge)
+        {
+            InitializeComponent();
+            BindingContext = new SelectedFridgeViewModel(selectedFridge);
+        }
+    }
+}
