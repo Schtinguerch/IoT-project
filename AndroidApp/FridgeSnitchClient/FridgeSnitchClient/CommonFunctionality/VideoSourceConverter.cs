7276d376696febe5ab6832361adc778158d8816f
Schtinguerch <schtinguerch@gmail.com>
2022-11-10 20:22:47 +0500

    Конвертер строки в Uri для работы Binding'ов

---
+++ AndroidApp/FridgeSnitchClient/FridgeSnitchClient/CommonFunctionality/VideoSourceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using Xam.Forms.VideoPlayer;
+
+namespace FridgeSnitchClient.CommonFunctionality
+{
+    public class VideoSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new UriVideoSource()
+            {
+                Uri = (string) value,
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
