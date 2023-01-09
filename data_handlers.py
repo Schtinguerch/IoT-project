d71c7b89a1ab1569d13a7c7657b132bc44d65104
unknown <oznobixin.el@yandex.ru>
2023-01-10 00:49:00 +0500

    + чтение файла настроеек, сообщение об успешном выполнении

--- data_handlers.py
+++ data_handlers.py
@@ -8,3 +8,9 @@ folder_exists = os.path.exists(folder)
 
 if not folder_exists:
     os.makedirs(folder)
+
+f = open('data.json', "r")
+data = json.loads(f.read())
+
+f.close()
+print("JSON Data initialized")
