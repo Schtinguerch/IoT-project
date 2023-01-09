49115a1bc8b70b71e29a6a7ef991b1cabca0ede0
unknown <oznobixin.el@yandex.ru>
2023-01-10 00:54:07 +0500

    + команда старта работы с ботом, запись в базу пользователей

--- telegram_bot_handlers.py
+++ telegram_bot_handlers.py
@@ -16,3 +16,14 @@ try:
 except Exception:
     print("Error: cannot create Telegram Bot's instance, check your connection and Api Key")
     exit(1)
+
+@tg_bot.message_handler(commands=["начать"])
+def on_start_command(message):
+    if message.from_user.id in data_handlers.data["TelegramUsers"]:
+        tg_bot.send_message(message.from_user.id, "Всё хорошо, я уже настроен и во всю работаю!!!")
+        return
+
+    data_handlers.data["TelegramUsers"].append(message.from_user.id)
+    data_handlers.save_data()
+
+    tg_bot.send_message(message.from_user.id, "Настройка прошла успешно, за сим я на страже!")
