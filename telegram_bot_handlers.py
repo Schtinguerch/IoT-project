9a723993798d01c41fbc9d507a20f9c877997816
unknown <oznobixin.el@yandex.ru>
2023-01-10 00:52:05 +0500

    + попытка запустить бота по токену, + сообщение об ошибке в случае провала

--- telegram_bot_handlers.py
+++ telegram_bot_handlers.py
@@ -8,3 +8,11 @@ print("Start creating Telegram Bot instance")
 if not ("TelegramApiKey" in data_handlers.data):
     print("First app launch, please insert your Telegram Bot API token inside data.json in ApiKeys property")
     exit(0)
+
+try:
+    tg_bot = telebot.TeleBot(data_handlers.data["TelegramApiKey"])
+    tg_bot.set_my_commands(commands.command_dictionary)
+
+except Exception:
+    print("Error: cannot create Telegram Bot's instance, check your connection and Api Key")
+    exit(1)
