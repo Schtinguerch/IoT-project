2bd9c95827de07ac77510a322ff3adda026d712f
unknown <schtinguerch@gmail.com>
2023-01-10 01:17:59 +0500

    Del Discord-бот (critical defect)

--- thread_handlers.py
+++ thread_handlers.py
@@ -3,7 +3,6 @@ import motion_detector
 
 
 class Threader:
-    def __init__(self, telegram_bot, discord_bot, json_data):
+    def __init__(self, telegram_bot, json_data):
         self.tg_bot = telegram_bot
-        self.ds_bot = discord_bot
         self.data = json_data
