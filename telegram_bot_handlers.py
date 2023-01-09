450a930d55a14f5774067183dabf55f9ed347028
unknown <oznobixin.el@yandex.ru>
2023-01-10 00:51:11 +0500

    + проверка первого запуска, просьба вбить токен бота

--- telegram_bot_handlers.py
+++ telegram_bot_handlers.py
@@ -3,3 +3,8 @@ import commands
 import camera_handlers
 import data_handlers
 
+
+print("Start creating Telegram Bot instance")
+if not ("TelegramApiKey" in data_handlers.data):
+    print("First app launch, please insert your Telegram Bot API token inside data.json in ApiKeys property")
+    exit(0)
