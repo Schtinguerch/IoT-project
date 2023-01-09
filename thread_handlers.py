8ac4b05575ff0850eae4d5c4109995ab8e460da3
unknown <schtinguerch@gmail.com>
2023-01-10 01:19:04 +0500

    Add методы запуска бота и детектора движения

--- thread_handlers.py
+++ thread_handlers.py
@@ -6,3 +6,11 @@ class Threader:
     def __init__(self, telegram_bot, json_data):
         self.tg_bot = telegram_bot
         self.data = json_data
+
+    def launch_detector(self):
+        detector = motion_detector.InvasionDetector(self.tg_bot, self.ds_bot, self.data)
+        detector.start_monitoring()
+
+    def launch_telegram_listener(self):
+        self.tg_bot.polling(none_stop=True, interval=0)
+        
