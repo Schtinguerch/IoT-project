c8e2b88dc0a5450c05d0f8ecbcb508b4b10388ef
unknown <schtinguerch@gmail.com>
2023-01-10 01:20:03 +0500

    Add запуск бота и детектора движения в отдельных потоках

--- thread_handlers.py
+++ thread_handlers.py
@@ -13,4 +13,14 @@ class Threader:
 
     def launch_telegram_listener(self):
         self.tg_bot.polling(none_stop=True, interval=0)
-        
+
+    def launch_all_bots(self):
+        print("Start thread pool")
+        telegram_listener_thread = threading.Thread(target=self.launch_telegram_listener)
+        telegram_listener_thread.start()
+
+        motion_detector_thread = threading.Thread(target=self.launch_detector)
+        motion_detector_thread.start()
+
+        self.launch_discord_listener()
+        print("Thread pool created")
